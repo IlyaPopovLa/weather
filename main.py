34dcd3d283ea8c7c6abf92469110ef1aa59dea38
IlyaPopovLa <il.popov@lamoda.ru>
2025-03-02 19:23:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ LOCATIONS = ["Лондон", "Шереметьево", "Череповец"]
 
 
 def get_weather(location_list):
+    weather_info = []
+    lang = 'ru'
     for location in location_list:
-        lang = 'ru'
-        params = {'nTqu': '', 'lang': lang}
         url = f"https://wttr.in/{location}"
+        params = {'nTqu': '', 'lang': lang}
         response = requests.get(url, params=params)
-        print(response.text)
-
+        response.raise_for_status()
+        weather_info.append(response.text)
+    return weather_info
 
 def main():
-    get_weather(LOCATIONS)
+    weather_data = get_weather(LOCATIONS)
+    for weather in weather_data:
+        print(weather)
 
 
 if __name__ == "__main__":
